e3c9049e0305ecfb1ba31d8e498b1d766272ffec
Allarassem Maxime <allarassemmaxime@Allarassems-MacBook-Air.local>
2025-07-24 01:25:11 +0300

    Implementing the dark mode, and the login and register

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,47 +1,39 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Sidebar from "@/components/Sidebar";
 import { Toaster } from "@/components/ui/sonner"
-
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Admin Dashboard",
-  description: "Reactjs Nextjs Typescript Admin Dashboard",
+    title: "Admin Dashboard",
+    description: "Reactjs Nextjs Typescript Admin Dashboard",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar />
-        <div className="flex">
-            <div className="hidden md:block h-[100vh] w-[300px]">
-                <Sidebar />
-            </div>
-            <div className="p-5 w-full md:max-w-[1140px]">
-                {children}
-            </div>
-            <Toaster />
-        </div>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                <ThemeProvider attribute='class' defaultTheme="light" enableSystem={true} storageKey="dashboard-theme">
+                    { children }
+                    <Toaster />
+                </ThemeProvider>
+            </body>
+        </html>
+    );
 }
